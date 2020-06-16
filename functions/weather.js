255e94b0486ebd2cc776d73e3a4a11de101b2b61
vehikl <vehikl@vehikl.com>
2020-06-16 17:00:12 -0400

    Connect weather API with Slack API

--- functions/weather.js
+++ functions/weather.js
@@ -1,6 +1,46 @@
-exports.handler = function(event, context, callback) {
+// @ts-check
+const fetch = require('node-fetch').default;
+
+async function getWeather(city) {
+    const weather =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_KEY}`).then((res) => res.json());
+    return weather;
+}
+
+exports.handler = async function(event, context, callback) {
+    const weather = await getWeather('Waterloo,ca');
+
+    function display(body) {
+        return `The weather is ${body.main.temp} in ${body.name}`
+    }
+
+    const res = await fetch(`https://slack.com/api/chat.postMessage`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.TOKEN}`,
+            'Content-Type': 'application/json'
+          }, 
+        body: JSON.stringify({
+            channel: "CEPE43JQ0",
+            blocks: [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `:sunny: ${display(weather)}`
+                    },
+                    "accessory": {
+                        "type": "image",
+                        "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+                        "alt_text": "cute cat"
+                    }
+                }
+            ]
+        })
+    })
+
     callback(null, {
         statusCode: 200,
-        body: "Hello, World"
+        body: "success"
     });
 }
+
