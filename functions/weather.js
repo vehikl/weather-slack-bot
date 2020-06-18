81f81a921ee476f59450a216ebcdac236cbd514e
vehikl <vehikl@vehikl.com>
2020-06-18 16:52:47 -0400

    Provide image in accordance to the weather

--- functions/weather.js
+++ functions/weather.js
@@ -6,11 +6,11 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.TOKEN}`;
 
 exports.handler = async function (event, context, callback) {
     const parsed = new URLSearchParams(event.body);
-
     const city = parsed.get('text') || '';
+
     try {
         const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_KEY}`);
-
+        const iconUrl = getWeatherIcon(weather.data);
         await axios.post(
             "https://slack.com/api/chat.postMessage",
             {
@@ -24,23 +24,25 @@ exports.handler = async function (event, context, callback) {
                         },
                         "accessory": {
                             "type": "image",
-                            "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+                            "image_url": iconUrl,
                             "alt_text": "cute cat"
                         }
                     }
                 ]
             });
     } catch (err) {
-        await axios.post(
-            "https://slack.com/api/chat.postMessage",
-            {
-                channel: "CEPE43JQ0",
-                text: "Could not find city " + city
-            });
+        await axios.post("https://slack.com/api/chat.postMessage", {
+            channel: "CEPE43JQ0",
+            text: "Could not find city " + city
+        });
     }
 
     function display(body) {
         return `The weather is ${body.main.temp} in ${body.name}`
+    }
+
+    function getWeatherIcon(body) {
+        return `http://openweathermap.org/img/wn/${body.weather[0].icon}@2x.png`
     }
 
     callback(null, { statusCode: 204, body: '' });
