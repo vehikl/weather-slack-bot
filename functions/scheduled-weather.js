e8042cfeea6e187fba0ec90e6750b3b864cbb320
Tyler Bauman <t.bauman@vehikl.com>
2020-07-08 18:15:42 -0400

    feat(daily-forecast): add new layout for showing morning/afternoon forecast

--- functions/scheduled-weather.js
+++ functions/scheduled-weather.js
@@ -1,48 +1,93 @@
 // @ts-check
 const axios = require('axios').default;
-axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.TOKEN}`;
+const { CHANNEL, TOKEN, WEATHER_KEY } = process.env
+axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`;
 
-function displayText(body) {
-  const imageToEmoji = {
-    '01d': ':sunny:',
-    '01n': ':moon:',
-    '02d': ':sun_small_cloud:',
-    '02n': ':cloud:',
-    '03d': ':cloud:',
-    '03n': ':cloud:',
-    '04d': ':cloud:',
-    '04n': ':cloud:',
-    '10d': ':rain_cloud:',
-    '10n': ':rain_cloud:',
-    '11d': ':thunder_cloud_and_rain:',
-    '11n': ':thunder_cloud_and_rain:',
-    '13d': ':snowflake:',
-    '13n': ':snowflake:',
-    '50d': ':fog:',
-    '50n': ':fog:'
-  };
+const imageToEmoji = {
+  '01d': ':sunny:',
+  '01n': ':moon:',
+  '02d': ':sun_small_cloud:',
+  '02n': ':cloud:',
+  '03d': ':cloud:',
+  '03n': ':cloud:',
+  '04d': ':cloud:',
+  '04n': ':cloud:',
+  '10d': ':rain_cloud:',
+  '10n': ':rain_cloud:',
+  '11d': ':thunder_cloud_and_rain:',
+  '11n': ':thunder_cloud_and_rain:',
+  '13d': ':snowflake:',
+  '13n': ':snowflake:',
+  '50d': ':fog:',
+  '50n': ':fog:'
+};
 
-  return `${imageToEmoji[body.weather[0].icon]}  The weather is ${body.main.temp}°C and ${body.weather[0].description} in ${body.name}.`;
+const getCityWeather = async () => {
+  const offices = [];
+
+  const officeCoords = [ 
+    { name: 'london', latitude: 42.98, longitude: -81.25 }, 
+    { name: 'waterloo', latitude: 43.47, longitude: -80.52 }, 
+    { name: 'hamilton', latitude: 43.26, longitude: -79.84 }, 
+    { name: 'sudbury', latitude: 46.50, longitude: -80.99 } 
+  ];
+
+  await Promise.all(
+    officeCoords.map(async ({name, latitude, longitude}) => {
+      const EXCLUDE = 'exclude=current,hourly,minutely';
+    
+      const { data: { daily: [ today ] } } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&${EXCLUDE}&units=metric&appid=${WEATHER_KEY}`
+      );
+    
+      offices.push({ city: name.toUpperCase(), ...today })
+    })
+  );
+
+  return offices;
 }
 
 exports.handler = async function (event, context, callback) {  
-  const offices = [6176823, 6058560, 5969785, 5964700];
-  const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/group?id=${offices}&units=metric&appid=${process.env.WEATHER_KEY}`)
+  const data = await getCityWeather();
 
   try {
     await axios.post('https://slack.com/api/chat.postMessage', {
-      channel: process.env.CHANNEL,
+      channel: CHANNEL,
       blocks: [ 
-        ...data.list.map((city) => ({ type: 'section', text: { type: 'mrkdwn', text: displayText(city) } })),
-        { type: 'divider' },
-        { type: 'section', text: { type: 'mrkdwn', text: `*_You should probably play it safe and stay inside!_* :colin_kent:` } }
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Today's weather in the greater Vehikl area. \n *_You should probably play it safe and stay inside!_* :colin_kent:"
+          }
+        },
+        { "type": "divider" },
+        ...data.map(({ city, temp, weather, feels_like}) => ({
+          type: 'section', 
+          text: {
+            "text": `*${city}* _High: ${temp.max}°C / Low: ${temp.min}°C_ \n Current weather conditions ${weather[0].description} `,
+            "type": "mrkdwn"
+          },
+          fields: [
+            { "type": "mrkdwn", "text": "*Morning*" },
+            { "type": "mrkdwn", "text": "*Feels Like*" },
+            { "type": "plain_text", "text": `${imageToEmoji[weather[0].icon]} ${temp.morn}°C` },
+            { "type": "plain_text", "text": `${feels_like.morn}°C` },
+            { "type": "mrkdwn", "text": "*Afternoon*" },
+            { "type": "mrkdwn", "text": "*Feels Like*" },
+            { "type": "plain_text", "text": `${imageToEmoji[weather[0].icon]} ${temp.eve}°C` },
+            { "type": "plain_text", "text": `${feels_like.morn}°C` }
+          ],
+          accessory: {
+            "type": "image",
+            "image_url": "https://cdn.dribbble.com/users/915711/screenshots/5827243/weather_icon3.png",
+            "alt_text": "weather thumbnail"
+          }
+        }))
       ]
     });
   } catch (err) {
-    await axios.post('https://slack.com/api/chat.postMessage', {
-      channel: process.env.CHANNEL,
-      text: `:no_entry: Error: office body not well formed`
-    });
+    await axios.post('https://slack.com/api/chat.postMessage', { channel: CHANNEL, text: `:no_entry: Error: office body not well formed` });
   }
 
   callback(null, { statusCode: 204, body: '' });
